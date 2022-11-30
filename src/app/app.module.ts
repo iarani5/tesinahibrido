25ddf6bf64538830662ba95d23c945eab7b69330
Iara <iaranizza5@gmail.com>
2022-11-30 18:55:53 +0100

    saved data from json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -3,15 +3,16 @@ import { ActivatedRoute } from '@angular/router';
 
 //photo
 import { PhotoService } from '../services/photo.service';
-import { Storage } from '@ionic/storage-angular';
-
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Preferences, SetOptions, GetOptions, RemoveOptions } from '@capacitor/preferences';
+
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
+
 export class FolderPage implements OnInit {
   public folder: string;
   user: ProfileComponent;
@@ -21,7 +22,6 @@ export class FolderPage implements OnInit {
     private activatedRoute: ActivatedRoute,
     public photoService: PhotoService,
     public formBuilder: FormBuilder,
-    private storage: Storage,
   ) {
 
     this.todo = this.formBuilder.group({
@@ -39,15 +39,57 @@ export class FolderPage implements OnInit {
 
   }
 
-  logForm() {
-    console.log(this.todo.value);
+  
+
+  async logForm() {
+
+    //console.log(this.photoService);
+    const name = 'user_pet' + Date.now();
+
+    let options: SetOptions = {
+      key: name,
+      value: JSON.stringify(this.todo.value)
+    };
+
+    Preferences.set(options);
+
+    /*const setName = async () => {
+      await Preferences.set({
+        key: name,
+        value: this.todo.value,
+      });
+    };*/
+
+    //const rta = await Preferences.get({ key: name });
+
+    console.log("RTAAAAA ------------------");
+
+    /*const checkName = async () => {
+      //const { value } = await Preferences.get({ key: name });
+      //console.log(`${value}`);
+
+      const rta = await Preferences.get({ key: name });
+      console.log(rta);
+    };*/
+
+
+    //this.todo.value
+    //var name = 'user_pet' + Date.now();
+    //await this.storage.set('user_pet_pics', this.photoService);
+
+    this.saved(name);
   }
 
+  async saved(name) {
+    const rta = await Preferences.get({ key: name });
+    const user_data = JSON.parse(rta.value);
+    console.log(user_data.situacion);
+
+  }
 
   async ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
     await this.photoService.loadSaved();
-    await this.storage.create();
   }
 
   addPhotoToGallery() {
