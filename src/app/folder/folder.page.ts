6e5bb7f5fcecd97dd9e01d94bb8a786e7e9eb987
Iara <iaranizza5@gmail.com>
2023-01-11 18:32:49 -0300

    db added

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,10 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+//modal
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 //photo
 import { PhotoService } from '../services/photo.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Preferences, SetOptions, GetOptions, RemoveOptions } from '@capacitor/preferences';
+import { async } from 'rxjs';
+
+//bdd
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfjTL5pun-50H3KVGIryYVqnCD2jsA3Eg",
+  authDomain: "tesina-hibrido.firebaseapp.com",
+  projectId: "tesina-hibrido",
+  storageBucket: "tesina-hibrido.appspot.com",
+  messagingSenderId: "351697777940",
+  appId: "1:351697777940:web:ea9eaf190024168c1efe01",
+  measurementId: "G-0797821TNE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 
 @Component({
@@ -14,15 +51,26 @@ import { Preferences, SetOptions, GetOptions, RemoveOptions } from '@capacitor/p
 })
 
 export class FolderPage implements OnInit {
+
+  @ViewChild(IonModal) modal: IonModal;
+
+  public user_data;
+  public message;
+  checkStatus: boolean;
   public folder: string;
   user: ProfileComponent;
   private todo: FormGroup;
+  public name = 'user_pet' + Date.now();
+  private savedImageFile;
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
     public photoService: PhotoService,
     public formBuilder: FormBuilder,
   ) {
+
+    this.checkStatus = false;
 
     this.todo = this.formBuilder.group({
       situacion: ['', Validators.required],
@@ -37,63 +85,89 @@ export class FolderPage implements OnInit {
 
     });
 
+    this.todo.value.castrado = true;
+
   }
 
   
 
   async logForm() {
 
-    //console.log(this.photoService);
-    const name = 'user_pet' + Date.now();
-
-    let options: SetOptions = {
-      key: name,
-      value: JSON.stringify(this.todo.value)
-    };
-
-    Preferences.set(options);
-
-    /*const setName = async () => {
-      await Preferences.set({
-        key: name,
-        value: this.todo.value,
-      });
-    };*/
-
-    //const rta = await Preferences.get({ key: name });
-
-    console.log("RTAAAAA ------------------");
-
-    /*const checkName = async () => {
-      //const { value } = await Preferences.get({ key: name });
-      //console.log(`${value}`);
-
-      const rta = await Preferences.get({ key: name });
-      console.log(rta);
-    };*/
-
-
-    //this.todo.value
-    //var name = 'user_pet' + Date.now();
     //await this.storage.set('user_pet_pics', this.photoService);
+    /*console.log(this.photoService);
 
-    this.saved(name);
+    if (this.savedImageFile != undefined) {
+
+    }*/
+   
+    try {
+
+      const docRef = await addDoc(collection(db, this.name), {
+        situacion: this.todo.value.situacion,
+        especie: this.todo.value.especie,
+        castrado: this.todo.value.castrado,
+        raza: this.todo.value.raza,
+        comentarios: this.todo.value.comentarios,
+        nombre: this.todo.value.nombre,
+        apellido: this.todo.value.apellido,
+        email: this.todo.value.email,
+        celular: this.todo.value.celular,
+        foto: this.savedImageFile
+
+      });
+
+      this.save();
+
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+   
+
   }
 
-  async saved(name) {
-    const rta = await Preferences.get({ key: name });
-    const user_data = JSON.parse(rta.value);
-    console.log(user_data.situacion);
+  async save() {
+
+    const querySnapshot = await getDocs(collection(db, this.name));
+    querySnapshot.forEach((doc) => {
+      if (doc.data()) {
+        this.checkStatus = true;
+        this.message = doc.data();
+
+        if (this.message.castrado || this.message.castrado == "") {
+          this.message.castrado = "Si";
+        }
+        else {
+          this.message.castrado = "No";
+        }
+
+        switch (this.message.situacion) {
+          case "en_adopcion":
+            this.message.situacion = "en adopción";
+            break;
+          case "en_transito":
+            this.message.situacion = "en transito";
+            break;
+        }
+
+        this.message.apellido = this.message.apellido.charAt(0).toUpperCase() + this.message.apellido.slice(1); 
+        this.message.nombre = this.message.nombre.charAt(0).toUpperCase() + this.message.nombre.slice(1); 
+        this.message.raza = this.message.raza.charAt(0).toUpperCase() + this.message.raza.slice(1); 
+      }
+
+      });
 
   }
 
-  async ngOnInit() {
+  async ngOnInit() { 
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-    await this.photoService.loadSaved();
+    await this.photoService.loadSaved(this.name); // key for user photos
   }
 
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+  async addPhotoToGallery() {
+    this.savedImageFile = await this.photoService.addNewToGallery(); //aca esta la ruta y el nombre de las fotos
+
+    console.log(this.savedImageFile);
   }
 
   /*
@@ -104,8 +178,25 @@ export class FolderPage implements OnInit {
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
   }*/
-   
 
+
+
+  //***** MODAL CONTROLLER *****//
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  goBack() {
+    this.todo = null;
+    this.checkStatus = false;
+  }
+  /*onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }*/
 }
 
 
@@ -121,3 +212,7 @@ export interface ProfileComponent {
   celular: number;
 }
 
+
+function setObject() {
+    throw new Error('Function not implemented.');
+}
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Platform } from '@ionic/angular'; //used to retrieve information about the current device
 
 
+import { FolderPage } from '../folder/folder.page';
+
+
 //photo
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
@@ -15,14 +18,21 @@ import { Capacitor } from '@capacitor/core';
 export class PhotoService {
 
   public photos: UserPhoto[] = [];
-  private PHOTO_STORAGE: string = 'photos';
+  private PHOTO_STORAGE: string =  'photos'; 
   private platform: Platform;
+  public name_key_user: string; //id with photos from user   
 
-  constructor(platform: Platform) {
+  constructor(
+
+    platform: Platform,
+   // public folderPage: FolderPage //used to set a user id in order to generate a key to save photos
+
+  ) { 
     this.platform = platform;
   }
 
   public async addNewToGallery() {
+
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -31,37 +41,23 @@ export class PhotoService {
     });
 
     // Save the picture and add it to photo collection
-  const savedImageFile = await this.savePicture(capturedPhoto);
-  this.photos.unshift(savedImageFile);
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
 
-
-
-
-    //-------------------------- ES PRUEBA HAY Q BORRARLO
-    /*const readFile = await Filesystem.readFile({
-      path: ,
-      directory: Directory.Data
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: `data:image/jpeg;base64,${readFile.data}`;
-    });*/ 
-    //--------------------------
+    console.log(savedImageFile); //esta guarando  {"__zone_symbol__state":null,"__zone_symbol__value":[]} el valor varia
 
-
-
-
-  Preferences.set({
-    key: this.PHOTO_STORAGE,
-    value: JSON.stringify(this.photos),
-  });
-
+    return JSON.stringify(savedImageFile);
   }
 
-  public async loadSaved() {
+  public async loadSaved(name) {
      // Retrieve cached photo array data
-    const photoList = await Preferences.get({ key: this.PHOTO_STORAGE });
+    //const photoList = await Preferences.get({ key: this.PHOTO_STORAGE });
+    const photoList = await Preferences.get({ key: name });
     this.photos = JSON.parse(photoList.value) || [];
 
     // Easiest way to detect when running on the web:
